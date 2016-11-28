1236c9792482248a8587a9b032ff52f9f5688441
Daniel Reigada <daniel.reigada@gmail.com>
2016-11-28 19:47:15 +0000

    Connect to both docker and sigma DB's

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 
 docker rm lampBD -f &> /dev/null
 
-docker run -d -e PASS="letmein" -p 80:80 -p 3306:3306 -v "$PWD":/app \
+docker run -d -e PASS="letmein" -e DOCKER_DEV="true" -p 80:80 -p 3306:3306 -v "$PWD":/app \
     -v "$PWD"/scripts/mysql-setup.sh:/mysql-setup.sh \
     --name lampBD \
     dreigada/lamp
--- web/DB.php
+++ web/DB.php
@@ -1,8 +1,15 @@
 <?php
 
-$host = "localhost";
-$user = "admin";
-$password = "letmein";
-$dbname = "BDdev";
+if (getenv("DOCKER_DEV")) {
+    $host = "localhost";
+    $user = "admin";
+    $password = "letmein";
+    $dbname = "BDdev";
+} else {
+    $host = "db.ist.utl.pt";
+    $user = "ist182064";        // -> substituir pelo nome de utilizador
+    $password = "xxxxxxxxx";     // -> substituir pela password
+    $dbname = $user;
+}
 
 $connection = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
