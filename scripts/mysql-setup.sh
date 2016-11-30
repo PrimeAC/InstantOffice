6c260a7444d9221b34c2084b2472b4445b0f6dc8
Daniel Reigada <daniel.reigada@gmail.com>
2016-11-30 17:11:04 +0000

    Run sql in run script

--- scripts/mysql-setup.sh
+++ scripts/mysql-setup.sh
@@ -2,3 +2,7 @@
 
 echo "CREATE DATABASE BDdev;" > test.sql
 mysql -uadmin -p"$PASS" -hlocalhost < test.sql
+
+mysql -uadmin -p"$PASS" -hlocalhost BDdev < /sql/schema.sql
+mysql -uadmin -p"$PASS" -hlocalhost BDdev < /sql/storedprocstriggers.sql
+mysql -uadmin -p"$PASS" -hlocalhost BDdev < /sql/populate.sql
