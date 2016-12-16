924c59215f56748ac02542de4f0b057b12b1804e
PrimeAC <afonso.caetano@outlook.com>
2016-12-16 01:06:45 +0000

    Reserva_Factos fully implemented

--- populateDW.sql
+++ populateDW.sql
@@ -5,11 +5,9 @@ TRUNCATE Localizacao;
 TRUNCATE Reserva_Factos;
 SET FOREIGN_KEY_CHECKS = 1;
 
-
 CALL load_date_dim();
 
 CALL load_time_dim();
-
 
 INSERT INTO Localizacao (espaco, edificio) (
   SELECT
@@ -24,13 +22,31 @@ codigo_espaco,
 morada
 FROM Posto);
 
-SELECT date_id FROM Data NATURAL JOIN Paga;
-
-SELECT time_id FROM Tempo NATURAL JOIN (SELECT MINUTE(data) + HOUR(data) * 60 AS time_of_day FROM Paga) A;
-
-SELECT nif FROM Aluga NATURAL JOIN Paga GROUP BY nif;
-
-SELECT DATEDIFF(data_fim, data_inicio) * tarifa AS value FROM Paga NATURAL JOIN Aluga NATURAL JOIN Oferta;
-
-SELECT DATEDIFF(data_fim, data_inicio) AS duration FROM Paga NATURAL JOIN Aluga NATURAL JOIN Oferta;
+INSERT INTO Reserva_Factos (reserva_id, date_id, time_id, location_id, nif, value, duration) (
+  SELECT
+    numero,
+    date_id,
+    time_id,
+    location_id,
+    nif,
+    tarifa,
+    DATEDIFF(data_fim, data_inicio) + 1
+  FROM Paga P
+    NATURAL JOIN Aluga
+    NATURAL JOIN Oferta O
+    NATURAL LEFT JOIN Posto
+    JOIN Localizacao L
+      ON L.edificio = O.morada AND
+         (L.espaco = O.codigo AND
+          L.posto IS NULL) OR
+         (L.posto = O.codigo AND
+          L.espaco = Posto.codigo_espaco)
+    JOIN Data D
+      ON D.date_day = DAY(P.data) AND
+         D.date_month_number = MONTH(P.data) AND
+         D.date_year = YEAR(P.data)
+    JOIN Tempo T
+      ON T.hour_of_day = HOUR(P.data) AND
+         T.minute_of_day = MINUTE(P.data)
+);
 
